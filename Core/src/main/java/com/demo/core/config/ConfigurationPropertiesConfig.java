0d1bd33856a924a23ba1e23090862c629090c404
김민준 <sanbonai06@naver.com>
2023-05-12 20:41:55 +0900

    refactor: delete kakao oidc

--- Core/src/main/java/com/demo/core/config/ConfigurationPropertiesConfig.java
+++ Core/src/main/java/com/demo/core/config/ConfigurationPropertiesConfig.java
@@ -2,10 +2,9 @@ package com.demo.core.config;
 
 
 import com.demo.core.properties.JwtProperties;
-import com.demo.core.properties.OauthProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({JwtProperties.class, OauthProperties.class})
+@EnableConfigurationProperties({JwtProperties.class})
 @Configuration
 public class ConfigurationPropertiesConfig {}
