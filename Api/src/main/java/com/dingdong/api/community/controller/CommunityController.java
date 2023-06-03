d8e6c2dd2469b81ae531d5b4205a6402cdc00c40
dkswnkk <jjoy9810@gmail.com>
2023-06-04 02:48:21 +0900

    feat: page 템플릿 변경

--- Api/src/main/java/com/dingdong/api/community/controller/CommunityController.java
+++ Api/src/main/java/com/dingdong/api/community/controller/CommunityController.java
@@ -3,12 +3,8 @@ package com.dingdong.api.community.controller;
 
 import com.dingdong.api.community.controller.response.CommunityIdCardsResponse;
 import com.dingdong.api.community.controller.response.CommunityListResponse;
-import com.dingdong.api.community.dto.CommunityIdCardsDto;
-import com.dingdong.api.global.dto.PagesDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +24,7 @@ public class CommunityController {
     @Operation(summary = "행성의 모든 주민증 목록 조회")
     @GetMapping("/{communityId}/idCards")
     public CommunityIdCardsResponse getCommunityIdCards(@PathVariable Long communityId) {
-        List<CommunityIdCardsDto> idCardsList = List.of(new CommunityIdCardsDto());
-        PagesDto pages = new PagesDto(new PageImpl<>(idCardsList));
-        return new CommunityIdCardsResponse(idCardsList, pages);
+
+        return new CommunityIdCardsResponse();
     }
 }
--- Api/src/main/java/com/dingdong/api/community/controller/response/CommunityIdCardsResponse.java
+++ Api/src/main/java/com/dingdong/api/community/controller/response/CommunityIdCardsResponse.java
@@ -2,18 +2,18 @@ package com.dingdong.api.community.controller.response;
 
 
 import com.dingdong.api.community.dto.CommunityIdCardsDto;
-import com.dingdong.api.global.dto.PagesDto;
+import com.dingdong.api.global.response.SliceResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import org.springframework.data.domain.Slice;
 
-@Data
-@AllArgsConstructor
+@Getter
 public class CommunityIdCardsResponse {
     @Schema(description = "주민증 목록")
-    private List<CommunityIdCardsDto> communityIdCardsDtos;
+    private SliceResponse<CommunityIdCardsDto> communityIdCardsDtos;
 
-    @Schema(description = "페이징 정보")
-    private PagesDto pages;
+    public static SliceResponse<CommunityIdCardsDto> from(
+            Slice<CommunityIdCardsDto> communityIdCardsDtos) {
+        return SliceResponse.from(communityIdCardsDtos);
+    }
 }
