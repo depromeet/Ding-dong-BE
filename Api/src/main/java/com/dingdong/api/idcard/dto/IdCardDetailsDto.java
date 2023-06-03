f9907ec786d2c4353f5189c0e5290d764075e025
dkswnkk <jjoy9810@gmail.com>
2023-06-04 01:02:39 +0900

    feat: IdCard 명세서 작성

---
+++ Api/src/main/java/com/dingdong/api/idcard/dto/IdCardDetailsDto.java
@@ -0,0 +1,28 @@
+package com.dingdong.api.idcard.dto;
+
+
+import com.dingdong.domain.domains.idcard.domain.enums.CharacterType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class IdCardDetailsDto {
+    private Long idCardId;
+
+    @Schema(description = "이름")
+    private String nickname;
+
+    @Schema(description = "유저 프로필 이미지")
+    private String profileImageUrl;
+
+    @Schema(description = "자기소개")
+    private String aboutMe;
+
+    @Schema(description = "키워드 정보")
+    private List<KeywordDto> keywords = new ArrayList<>();
+
+    @Schema(description = "캐릭터 타입")
+    private CharacterType characterType;
+}
