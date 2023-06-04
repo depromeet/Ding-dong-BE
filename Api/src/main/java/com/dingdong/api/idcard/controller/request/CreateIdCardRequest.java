6ec30c9aff9238bc02a0dae415072e21f1ca069d
김민준 <sanbonai06@naver.com>
2023-06-05 01:41:53 +0900

    feat: idCard 생성 API 스키마 정의

---
+++ Api/src/main/java/com/dingdong/api/idcard/controller/request/CreateIdCardRequest.java
@@ -0,0 +1,29 @@
+package com.dingdong.api.idcard.controller.request;
+
+import com.dingdong.api.idcard.dto.CreateKeywordDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateIdCardRequest {
+
+    @Schema(description = "행성에서 사용할 유저 닉네임", example = "김민준")
+    @NotNull(message = "행성에서 사용할 유저 닉네임을 입력해주세요.")
+    private String nickname;
+
+    @Schema(description = "유저 간단 자기소개", example = "안녕하세요. 김민준입니다.")
+    @NotNull(message = "유저 자기소개를 입력해주세요.")
+    @Size(max = 500, message = "자기소개는 500글자 제한입니다.")
+    private String aboutMe;
+
+    @Schema(description = "유저가 선택한 키워드와 키워드 설명글 리스트")
+    @NotNull(message = "적어도 한 개의 키워드는 선택해야 합니다.")
+    @Valid
+    private List<CreateKeywordDto> keywords;
+}
---
+++ Api/src/main/java/com/dingdong/api/idcard/dto/CreateKeywordDto.java
@@ -0,0 +1,20 @@
+package com.dingdong.api.idcard.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CreateKeywordDto {
+
+    @Schema(description = "키워드 제목", example = "안산러")
+    @NotNull(message = "키워드 이름을 입력해주세요")
+    private String title;
+
+    @Schema(description = "키워드 이미지", example = "test.com")
+    private String imageUrl;
+
+    @Schema(description = "키워드 내용", example = "안산에서 통학하는 불쌍한 영혼을 구제해주세요... 내일도 학교가야함")
+    @NotNull(message = "키워드 설명을 적어주세요")
+    private String content;
+}
