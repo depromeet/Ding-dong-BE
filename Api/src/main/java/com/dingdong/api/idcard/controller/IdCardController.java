82ac546227bdff63b86f5d9e99799f63a7fef374
Minjoon Kim <59060780+sanbonai06@users.noreply.github.com>
2023-06-05 10:27:03 +0900

    DEP-62: 주민증 생성 스키마 작성 (#12)
    * feat: idCard 생성 API 스키마 정의
    
    * feat: idCard 생성 API 스키마 communityId 추가
    
    * refactor: MethodArgumentNotValidException 에러 핸들러 추가
    
    * style: spotless
    
    * refactor: api 반환형 void로 수정 및 유저 닉네임 검증 NotBlank 추가
    
    * fix: NotNull 제거

--- Api/src/main/java/com/dingdong/api/idcard/controller/IdCardController.java
+++ Api/src/main/java/com/dingdong/api/idcard/controller/IdCardController.java
@@ -1,12 +1,16 @@
 package com.dingdong.api.idcard.controller;
 
 
+import com.dingdong.api.idcard.controller.request.CreateIdCardRequest;
 import com.dingdong.api.idcard.controller.response.CommentCountResponse;
 import com.dingdong.api.idcard.controller.response.IdCardDetailsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,11 @@ public class IdCardController {
     @GetMapping("/{idCardsId}/comment-count")
     public CommentCountResponse getCommentCount(@PathVariable Long idCardsId) {
         return new CommentCountResponse();
+    }
+
+    @Operation(summary = "주민증 생성")
+    @PostMapping
+    public void postIdCard(@RequestBody @Valid CreateIdCardRequest body) {
+        // Todo: 유저 id값은 나중에 로그인 기능 완성되고 AccessToken 필터 추가 되면 거기서 가져오기
     }
 }
