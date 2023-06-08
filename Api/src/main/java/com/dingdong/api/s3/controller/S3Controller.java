b1772516c9f80932c452a08f6803be54f26772a5
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-09 02:53:25 +0900

    style : 네이밍 오류 수정, spotless check

--- Api/src/main/java/com/dingdong/api/s3/controller/S3Controller.java
+++ Api/src/main/java/com/dingdong/api/s3/controller/S3Controller.java
@@ -1,5 +1,11 @@
 package com.dingdong.api.s3.controller;
 
+
+import com.dingdong.api.s3.controller.response.UploadImageResponse;
+import com.dingdong.infrastructure.s3.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,29 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.dingdong.api.s3.controller.response.UploadImageResponse;
-import com.dingdong.infrastructure.s3.S3Service;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-
 @Tag(name = "이미지 업로드")
 @RestController
 @RequestMapping("/upload")
 @RequiredArgsConstructor
 public class S3Controller {
-	private final S3Service s3Service;
 
-	@Operation(summary = "이미지 업로드")
-	@PostMapping("/image")
-	public UploadImageResponse uploadImage(@RequestParam("image") MultipartFile multipartFile) throws Exception {
-		return new UploadImageResponse(s3Service.uploadImage(multipartFile));
-	}
+    private final S3Service s3Service;
 
-	@Operation(summary = "이미지 삭제")
-	@DeleteMapping("/image")
-	public void remove(String fileUrl) throws Exception {
-		s3Service.remove(fileUrl);
-	}
+    @Operation(summary = "이미지 업로드")
+    @PostMapping("/image")
+    public UploadImageResponse uploadImage(@RequestParam("image") MultipartFile multipartFile)
+            throws Exception {
+        return new UploadImageResponse(s3Service.uploadImage(multipartFile));
+    }
+
+    @Operation(summary = "이미지 삭제")
+    @DeleteMapping("/image")
+    public void remove(String fileUrl) throws Exception {
+        s3Service.remove(fileUrl);
+    }
 }
--- Api/src/main/java/com/dingdong/api/s3/controller/response/UploadImageResponse.java
+++ Api/src/main/java/com/dingdong/api/s3/controller/response/UploadImageResponse.java
@@ -1,5 +1,6 @@
 package com.dingdong.api.s3.controller.response;
 
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UploadImageResponse {
-	@Schema(description = "업로드 한 이미지 url")
-	private String imageUrl;
+    @Schema(description = "업로드 한 이미지 url")
+    private String imageUrl;
 }
