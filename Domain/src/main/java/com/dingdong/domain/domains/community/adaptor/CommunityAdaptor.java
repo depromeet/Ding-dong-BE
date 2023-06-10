9916435f1aaa5a533fa7349aac943cc8d07e800c
dkswnkk <jjoy9810@gmail.com>
2023-06-10 16:07:58 +0900

    refactor: 메서드명 변경

--- Domain/src/main/java/com/dingdong/domain/domains/community/adaptor/CommunityAdaptor.java
+++ Domain/src/main/java/com/dingdong/domain/domains/community/adaptor/CommunityAdaptor.java
@@ -1,12 +1,12 @@
 package com.dingdong.domain.domains.community.adaptor;
 
-import static com.dingdong.domain.domains.community.exception.CommunityErrorCode.NOT_FOUND_COMMUNITY;
-
 import com.dingdong.core.annotation.Adaptor;
 import com.dingdong.core.exception.BaseException;
 import com.dingdong.domain.domains.community.domain.Community;
 import com.dingdong.domain.domains.community.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
+
+import static com.dingdong.domain.domains.community.exception.CommunityErrorCode.NOT_FOUND_COMMUNITY;
 
 @Adaptor
 @RequiredArgsConstructor
@@ -14,7 +14,7 @@ public class CommunityAdaptor {
 
     private final CommunityRepository communityRepository;
 
-    public Community find(Long communityId) {
+    public Community findById(Long communityId) {
         return communityRepository
                 .findById(communityId)
                 .orElseThrow(() -> new BaseException(NOT_FOUND_COMMUNITY));
