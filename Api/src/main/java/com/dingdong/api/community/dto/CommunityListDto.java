536e43620a4252f565ab570c228892de8b9d235f
dkswnkk <jjoy9810@gmail.com>
2023-06-13 23:46:59 +0900

    feat: 유저가 소속 되어있는 행성 목록 조회 API 구현

--- Api/src/main/java/com/dingdong/api/community/dto/CommunityListDto.java
+++ Api/src/main/java/com/dingdong/api/community/dto/CommunityListDto.java
@@ -1,10 +1,13 @@
 package com.dingdong.api.community.dto;
 
 
+import com.dingdong.domain.domains.community.domain.Community;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CommunityListDto {
     @Schema(description = "행성 Id")
     private Long communityId;
@@ -17,4 +20,13 @@ public class CommunityListDto {
 
     @Schema(description = "주민 수")
     private int idCardCount;
+
+    public static CommunityListDto from(Community community, int idCardCount) {
+        return CommunityListDto.builder()
+                .communityId(community.getId())
+                .logoImageUrl(community.getLogoImageUrl())
+                .title(community.getName())
+                .idCardCount(idCardCount)
+                .build();
+    }
 }
