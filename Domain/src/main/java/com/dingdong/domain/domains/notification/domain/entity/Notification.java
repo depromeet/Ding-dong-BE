fca700259b3df661d920074420eea1572ef4b7c0
JuHyeong Ahn <jjoy9810@gmail.com>
2023-06-20 22:54:26 +0900

    DEP-125: 알림 관련 DB 생성 (#32)
    * feat: 알림 관련 도메인 생성
    
    * feat: 알림 좋아요 타입 추가

---
+++ Domain/src/main/java/com/dingdong/domain/domains/notification/domain/entity/Notification.java
@@ -0,0 +1,42 @@
+package com.dingdong.domain.domains.notification.domain.entity;
+
+
+import com.dingdong.domain.domains.AbstractTimeStamp;
+import com.dingdong.domain.domains.notification.domain.enums.NotificationStatus;
+import com.dingdong.domain.domains.notification.domain.enums.NotificationType;
+import com.dingdong.domain.domains.notification.domain.vo.NotificationContent;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "tbl_notification")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends AbstractTimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NotificationType notificationType;
+
+    @Embedded private NotificationContent content;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NotificationStatus notificationStatus;
+}
