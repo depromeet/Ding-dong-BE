f717b86a9604c8b2f7aac5f68f90929291384eb6
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-23 16:07:17 +0900

    feat : Swagger 에러 코드 문서화 작업 (#36)

--- Api/src/main/java/com/dingdong/api/config/SwaggerConfig.java
+++ Api/src/main/java/com/dingdong/api/config/SwaggerConfig.java
@@ -1,20 +1,35 @@
 package com.dingdong.api.config;
 
+import static java.util.stream.Collectors.groupingBy;
 
+import com.dingdong.core.annotation.ApiErrorCodeExample;
+import com.dingdong.core.dto.ErrorDetail;
+import com.dingdong.core.dto.ErrorResponse;
+import com.dingdong.core.exception.BaseErrorCode;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.examples.Example;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletContext;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
 @Configuration
 @RequiredArgsConstructor
@@ -51,5 +66,73 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")
                                 .in(In.HEADER)
                                 .name("Authorization"));
+    }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            ApiErrorCodeExample apiErrorCodeExample =
+                    handlerMethod.getMethodAnnotation(ApiErrorCodeExample.class);
+            // ApiErrorCodeExample 어노테이션 단 메소드 적용
+            if (apiErrorCodeExample != null) {
+                generateErrorCodeResponseExample(operation, apiErrorCodeExample.value());
+            }
+            return operation;
+        };
+    }
+
+    private void generateErrorCodeResponseExample(
+            Operation operation, Class<? extends BaseErrorCode> type) {
+        ApiResponses responses = operation.getResponses();
+
+        BaseErrorCode[] errorCodes = type.getEnumConstants();
+
+        Map<Integer, List<ExampleHolder>> statusWithExampleHolders =
+                Arrays.stream(errorCodes)
+                        .map(
+                                baseErrorCode -> {
+                                    try {
+                                        ErrorDetail errorDetail = baseErrorCode.getErrorDetail();
+                                        return ExampleHolder.builder()
+                                                .holder(
+                                                        getSwaggerExample(
+                                                                baseErrorCode.getExplainError(),
+                                                                errorDetail))
+                                                .code(errorDetail.getStatusCode())
+                                                .name(errorDetail.getErrorCode())
+                                                .build();
+                                    } catch (NoSuchFieldException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                })
+                        .collect(groupingBy(ExampleHolder::getCode));
+
+        addExamplesToResponses(responses, statusWithExampleHolders);
+    }
+
+    private Example getSwaggerExample(String value, ErrorDetail errorDetail) {
+        ErrorResponse errorResponse = new ErrorResponse(errorDetail);
+        Example example = new Example();
+        example.description(value);
+        example.setValue(errorResponse);
+        return example;
+    }
+
+    private void addExamplesToResponses(
+            ApiResponses responses, Map<Integer, List<ExampleHolder>> statusWithExampleHolders) {
+        statusWithExampleHolders.forEach(
+                (status, v) -> {
+                    Content content = new Content();
+                    MediaType mediaType = new MediaType();
+                    ApiResponse apiResponse = new ApiResponse();
+                    v.forEach(
+                            exampleHolder -> {
+                                mediaType.addExamples(
+                                        exampleHolder.getName(), exampleHolder.getHolder());
+                            });
+                    content.addMediaType("application/json", mediaType);
+                    apiResponse.setContent(content);
+                    responses.addApiResponse(status.toString(), apiResponse);
+                });
     }
 }
