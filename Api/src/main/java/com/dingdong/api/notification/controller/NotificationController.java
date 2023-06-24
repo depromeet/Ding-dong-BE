56f4d6b955b74ea2d2ea96487efd301c1c060dd8
JuHyeong Ahn <jjoy9810@gmail.com>
2023-06-24 20:54:10 +0900

    DEP-115: SSE 구독 API 작업 (#39)
    * feat: 명세 수정
    
    * feat: 알림 구독 기능 구현

--- Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
+++ Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
@@ -2,35 +2,42 @@ package com.dingdong.api.notification.controller;
 
 
 import com.dingdong.api.notification.dto.NotificationDto;
+import com.dingdong.api.notification.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@SecurityRequirement(name = "access-token")
 @Tag(name = "알림")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/notifications")
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
     @Operation(summary = "알림 구독")
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe() {
-        return new SseEmitter();
+        return notificationService.subscribe();
     }
+
+    @Operation(summary = "알림 읽음 처리")
+    @GetMapping("/{notificationId}/read")
+    public void readNotification(@PathVariable Long notificationId) {}
 
     @Operation(summary = "알림 목록 조회")
     @GetMapping
-    public NotificationDto getNotifications(
-            @RequestParam(required = false, defaultValue = "null") Long communityId,
-            @PageableDefault Pageable pageable) {
+    public NotificationDto getNotifications(@PageableDefault Pageable pageable) {
         return new NotificationDto();
     }
 }
