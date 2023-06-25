6bdd93eb4228d48a7a3abcb548df1ff43d780e5b
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-25 12:01:59 +0900

    build : build fail 수정

--- Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
+++ Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @SecurityRequirement(name = "access-token")
