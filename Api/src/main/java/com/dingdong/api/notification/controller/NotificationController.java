dcd9c6ffa76580a57b82fc93acedebd409c5c32a
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-25 23:24:22 +0900

    style : spotless check

--- Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
+++ Api/src/main/java/com/dingdong/api/notification/controller/NotificationController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
