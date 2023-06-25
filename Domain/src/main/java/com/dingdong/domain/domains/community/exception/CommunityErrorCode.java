814b3abcb78ec37687fb98a5cfde0395bca6ffc4
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-25 23:29:05 +0900

    DEP-76: 행성 관련 API 수정 및 추가 (#41)
    * feat : 행성 만들기 / 꾸미기 API 명세 추가
    
    * refactor : 행성 이미지 관련 네이밍 수정
    
    * style : spotless check
    
    * refactor : @Valid 추가
    
    * style : spotless check
    
    * feat : 행성 만들기 / 행성 꾸미기 API 구현
    
    * refactor : 사용하지 않는 초대 코드 발번 로직 제거
    
    * refactor : 관리자 생성 및 추가 메서드 분리
    
    * Merge branch 'dev' into feat/DEP-76
    
    * refactor : 행성 만들기 / 꾸미기 리팩토링
    
    * refactor : 행성 초대 코드 발번 로직 전략 패턴 적용
    
    * fix : 충돌 오류 수정
    
    * refactor : 초대 코드 중복 체크 validator 메서드 추가
    
    * refactor : 함수형 인터페이스 선언, 구현체 메서드 수정
    
    * refactor : 코드 중복 체크 로직 변경 및 시간복잡도 개선
    
    * feat : 관리자의 행성 생성 시 행성 가입 처리
    
    * refactor : 행성 만들기/정보 조회 response 수정
    
    * feat : 행성 이름 중복 체크 API
    
    * refactor : 행성 이름 중복 체크 API response 변경
    
    * refactor : 중복 메서드 제거, 요청 method 변경
    
    * style : spotless check
    
    * refactor : code formatting
    
    * refactor : code formatting

--- Domain/src/main/java/com/dingdong/domain/domains/community/exception/CommunityErrorCode.java
+++ Domain/src/main/java/com/dingdong/domain/domains/community/exception/CommunityErrorCode.java
@@ -16,7 +16,13 @@ public enum CommunityErrorCode implements BaseErrorCode {
     @ExplainError("존재하지 않는 행성을 찾을 때 발생하는 오류입니다.")
     NOT_FOUND_COMMUNITY(NOT_FOUND, "Community-404-1", "존재하지 않는 행성입니다."),
     @ExplainError("관리자가 아닌 다른 사용자가 행성 꾸미기를 시도할 때 발생하는 오류입니다.")
-    NO_AUTHORITY_UPDATE_COMMUNITY(FORBIDDEN, "Community-403-1", "행성 수정 권한이 없습니다.");
+    NO_AUTHORITY_UPDATE_COMMUNITY(FORBIDDEN, "Community-403-1", "행성 수정 권한이 없습니다."),
+    @ExplainError("행성을 만들 때 이미 존재하는 행성 이름을 사용하려고 할 때 발생하는 오류입니다.")
+    ALREADY_EXIST_COMMUNITY_NAME(BAD_REQUEST, "Community-400-1", "이미 존재하는 행성 이름입니다."),
+    @ExplainError("행성 이름이 최소 길이인 1글자 미만일 때 발생하는 오류입니다.")
+    MIN_LIMIT_COMMUNITY_NAME(BAD_REQUEST, "Community-400-2", "행성 이름 최소 길이는 1글자입니다."),
+    @ExplainError("행성 이름이 최대 길이인 16글자를 초과할 때 발생하는 오류입니다.")
+    MAX_LIMIT_COMMUNITY_NAME(BAD_REQUEST, "Community-400-3", "행성 이름 최대 길이는 16글자입니다.");
 
     private final Integer statusCode;
     private final String errorCode;
--- Domain/src/main/java/com/dingdong/domain/domains/community/repository/CommunityRepository.java
+++ Domain/src/main/java/com/dingdong/domain/domains/community/repository/CommunityRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     boolean existsCommunityByInvitationCode(String invitationCode);
+
+    boolean existsCommunityByName(String name);
 }
