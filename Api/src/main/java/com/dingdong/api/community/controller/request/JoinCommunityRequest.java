1630aea2a0c98a07fd470fbc20351c62f294bc87
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-06-29 03:02:39 +0900

    docs : Swagger Request Body 설명 추가

--- Api/src/main/java/com/dingdong/api/community/controller/request/JoinCommunityRequest.java
+++ Api/src/main/java/com/dingdong/api/community/controller/request/JoinCommunityRequest.java
@@ -1,9 +1,11 @@
 package com.dingdong.api.community.controller.request;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class JoinCommunityRequest {
+    @Schema(description = "가입할 행성 ID", example = "1")
     private Long communityId;
 }
