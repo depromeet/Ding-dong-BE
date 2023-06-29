83a8b7a2d08b8dbe264f0a9823c66a1ebe287e32
dkswnkk <jjoy9810@gmail.com>
2023-06-30 00:29:35 +0900

    feat: 알림 생성 팩토리 추가

--- Domain/src/main/java/com/dingdong/domain/domains/notification/domain/model/NotificationContent.java
+++ Domain/src/main/java/com/dingdong/domain/domains/notification/domain/model/NotificationContent.java
@@ -2,16 +2,27 @@ package com.dingdong.domain.domains.notification.domain.model;
 
 
 import javax.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationContent {
     private Long communityId;
 
     private Long fromUserId;
 
     private Long commentId;
+
+    private NotificationContent(Long communityId, Long fromUserId, Long commentId) {
+        this.communityId = communityId;
+        this.fromUserId = fromUserId;
+        this.commentId = commentId;
+    }
+
+    public static NotificationContent create(Long communityId, Long fromUserId, Long commentId) {
+        return new NotificationContent(communityId, fromUserId, commentId);
+    }
 }
