e16035b0830f15557861c326d301fc73aec7d24d
dkswnkk <jjoy9810@gmail.com>
2023-07-05 23:31:24 +0900

    feat: select table 변경

--- Domain/src/main/java/com/dingdong/domain/domains/community/repository/CommunityRepositoryImpl.java
+++ Domain/src/main/java/com/dingdong/domain/domains/community/repository/CommunityRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.dingdong.domain.domains.community.repository;
 
-import static com.dingdong.domain.domains.community.domain.entity.QCommunity.community;
-import static com.dingdong.domain.domains.user.domain.QUser.user;
+import static com.dingdong.domain.domains.community.domain.entity.QUserJoinCommunity.userJoinCommunity;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +13,9 @@ public class CommunityRepositoryImpl implements CommunityRepositoryExtension {
     @Override
     public long userCountByCommunityId(Long communityId) {
         return queryFactory
-                .select(user.count())
-                .from(user)
-                .join(user.communities, community)
-                .where(community.id.eq(communityId))
+                .select(userJoinCommunity.count())
+                .from(userJoinCommunity)
+                .where(userJoinCommunity.communityId.eq(communityId))
                 .fetchOne();
     }
 }
