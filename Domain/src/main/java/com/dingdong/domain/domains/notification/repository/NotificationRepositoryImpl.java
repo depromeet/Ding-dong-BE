0d9403f2fd1bb9e5e48f82a6ebfca2b9dedf2ab0
dkswnkk <jjoy9810@gmail.com>
2023-07-07 22:07:18 +0900

    fix: 미확인 여부 알림 조회 쿼리 변경

--- Domain/src/main/java/com/dingdong/domain/domains/notification/repository/NotificationRepositoryImpl.java
+++ Domain/src/main/java/com/dingdong/domain/domains/notification/repository/NotificationRepositoryImpl.java
@@ -6,8 +6,11 @@ import static com.dingdong.domain.domains.idcard.domain.entity.QIdCard.idCard;
 import static com.dingdong.domain.domains.notification.domain.entity.QNotification.notification;
 
 import com.dingdong.domain.common.util.SliceUtil;
+import com.dingdong.domain.domains.notification.domain.entity.Notification;
+import com.dingdong.domain.domains.notification.domain.enums.NotificationStatus;
 import com.dingdong.domain.domains.notification.domain.vo.NotificationVO;
 import com.dingdong.domain.domains.notification.domain.vo.QNotificationVO;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -19,13 +22,27 @@ import org.springframework.data.domain.Slice;
 @RequiredArgsConstructor
 public class NotificationRepositoryImpl implements NotificationRepositoryExtension {
     private final JPAQueryFactory queryFactory;
+    private static final Timestamp FIVE_WEEKS_AGO =
+            Timestamp.valueOf(LocalDateTime.now().minusWeeks(5));
+
+    private JPAQuery<Notification> buildQuery(Long userId) {
+        return queryFactory
+                .selectFrom(notification)
+                .join(community)
+                .on(community.id.eq(notification.content.communityId))
+                .join(idCard)
+                .on(idCard.userInfo.userId.eq(notification.content.fromUserId))
+                .join(comment)
+                .on(notification.content.commentId.eq(comment.id))
+                .where(
+                        notification.userId.eq(userId),
+                        notification.createdAt.after(NotificationRepositoryImpl.FIVE_WEEKS_AGO));
+    }
 
     @Override
     public Slice<NotificationVO> findNotificationByConditionInPage(Long userId, Pageable pageable) {
-        Timestamp fiveWeeksAgo = Timestamp.valueOf(LocalDateTime.now().minusWeeks(5));
-
         List<NotificationVO> notificationVOs =
-                queryFactory
+                buildQuery(userId)
                         .select(
                                 new QNotificationVO(
                                         notification.id,
@@ -40,21 +57,21 @@ public class NotificationRepositoryImpl implements NotificationRepositoryExtensi
                                         community.id,
                                         comment.content,
                                         idCard.id))
-                        .from(notification)
-                        .where(
-                                notification.userId.eq(userId),
-                                notification.createdAt.after(fiveWeeksAgo))
-                        .join(community)
-                        .on(community.id.eq(notification.content.communityId))
-                        .join(idCard)
-                        .on(idCard.userInfo.userId.eq(notification.content.fromUserId))
-                        .join(comment)
-                        .on(notification.content.commentId.eq(comment.id))
                         .orderBy(notification.id.desc())
                         .offset(pageable.getOffset())
                         .limit(pageable.getPageSize() + 1)
                         .fetch();
 
         return SliceUtil.valueOf(notificationVOs, pageable);
+    }
+
+    @Override
+    public boolean existsUnreadNotification(Long userId) {
+        long count =
+                buildQuery(userId)
+                        .where(notification.notificationStatus.eq(NotificationStatus.UNREAD))
+                        .fetchCount();
+
+        return count > 0;
     }
 }
