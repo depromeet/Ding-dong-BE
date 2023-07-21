94d5ff1590e6aeceb81e72725ae133e3783e2442
dkswnkk <jjoy9810@gmail.com>
2023-07-21 18:47:17 +0900

    feat: 스케줄링 enable

---
+++ Api/src/main/java/com/dingdong/api/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.dingdong.api.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {}
